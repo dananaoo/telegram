7c7d66a79be0f5221d99d528c8837d210ef26fde
Dana <danazhaksylyk12@gmail.com>
2025-06-05 02:42:48 +0500

    setting updated

--- src/modules/settings/settings.tsx
+++ src/modules/settings/settings.tsx
@@ -6,5 +6,26 @@ export const Settings = () => {
       console.log("leave settings");
     };
   }, []);
-  return <div>settings</div>;
+
+  return (
+    <div className="flex flex-col h-full bg-white text-gray-900 p-5">
+      <header className="border-b border-gray-300 pb-3 mb-5">
+        <h1 className="text-xl font-semibold">Настройки</h1>
+      </header>
+      <section className="flex flex-col gap-4">
+        <div className="p-4 rounded hover:bg-gray-100 cursor-pointer">
+          Аккаунт
+        </div>
+        <div className="p-4 rounded hover:bg-gray-100 cursor-pointer">
+          Уведомления
+        </div>
+        <div className="p-4 rounded hover:bg-gray-100 cursor-pointer">
+          Конфиденциальность
+        </div>
+        <div className="p-4 rounded hover:bg-gray-100 cursor-pointer">
+          Чат и медиа
+        </div>
+      </section>
+    </div>
+  );
 };
